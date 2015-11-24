bfa7717a48ecaee481f4d64c22c45c7dca865f9c
jeffreymeng <jkmexpress@yahoo.com>
2015-11-24 09:47:59 -0800

    update

---
+++ client.js
@@ -0,0 +1,19 @@
+function addCDN(url, type, location) {
+	if (jQuery === undefined) {
+		throw "jeffrey api requires jQuery"
+	}
+	if (location === undefined) {
+		var newlocation = "head"
+	}
+	if (type === "js" || type === "javascript" || type === "JavaScript") {
+		var cdn = '<script src="' + url + '"></script>'
+	} else if (type === "css" || type === "CSS") {
+		var cdn = '<link rel="stylesheet" href="' + link '" type="text/css">'
+	} else {
+		throw "please specify a file type(js or css)"
+	}
+
+	if (cdn != undefined) {
+		$(newlocation).prepend(cdn)
+	}
+}
